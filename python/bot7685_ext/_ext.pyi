65fdeda0b1b3884969492bd5213795e3a58e8ea2
wyf7685 <wyf7685@163.com>
2026-05-25 17:33:51 +0800

    refactor: simplify LRU type hints

--- python/bot7685_ext/_ext.pyi
+++ python/bot7685_ext/_ext.pyi
@@ -1,13 +1,9 @@
 from asyncio import AbstractEventLoop
 from collections.abc import Mapping
 from types import GenericAlias
-from typing import Any, Callable, Generic, TypeVar, overload
+from typing import Any, Callable, overload
 
 from bot7685_ext.wplace.consts import ColorName
-
-_KT = TypeVar("_KT")
-_VT = TypeVar("_VT")
-_T = TypeVar("_T")
 
 __version__: str
 __build_time__: str
@@ -45,7 +41,7 @@ async def wplace_compose_tiles(
     /,
 ) -> bytes: ...
 
-class LRU(Generic[_KT, _VT]):
+class LRU[KT, VT]:
     """LRU dict that can store up to ``size`` elements.
 
     An LRU dict behaves like a standard dict, except that it stores only fixed
@@ -64,94 +60,70 @@ class LRU(Generic[_KT, _VT]):
     def __init__(
         self,
         size: int,
-        callback: Callable[[_KT, _VT], Any],
+        callback: Callable[[KT, VT], Any],
     ) -> None: ...
     def __class_getitem__(cls, item: Any, /) -> GenericAlias: ...
     def __repr__(self) -> str: ...
     def __len__(self) -> int: ...
-    def __contains__(self, key: _KT, /) -> bool: ...
-    def __getitem__(self, key: _KT, /) -> _VT: ...
-    def __setitem__(self, key: _KT, value: _VT, /) -> None: ...
-    def __delitem__(self, key: _KT, /) -> None: ...
+    def __contains__(self, key: KT, /) -> bool: ...
+    def __getitem__(self, key: KT, /) -> VT: ...
+    def __setitem__(self, key: KT, value: VT, /) -> None: ...
+    def __delitem__(self, key: KT, /) -> None: ...
     @overload
-    def get(self, key: _KT) -> _VT | None:
+    def get(self, key: KT) -> VT | None:
         """If L has key return its value, otherwise None."""
-        ...
     @overload
-    def get(self, key: _KT, instead: _T) -> _VT | _T:
+    def get[T](self, key: KT, instead: T) -> VT | T:
         """If L has key return its value, otherwise instead."""
-        ...
     @overload
-    def setdefault(self: LRU[_KT, _T | None], key: _KT) -> _VT | None:
+    def setdefault[T](self: LRU[KT, T | None], key: KT) -> VT | None:
         """If L has key return its value, otherwise insert key with a value of
         None and return None."""
-        ...
     @overload
-    def setdefault(self, key: _KT, default: _T) -> _VT | _T:
+    def setdefault[T](self, key: KT, default: T) -> VT | T:
         """If L has key return its value, otherwise insert key with a value of
         default and return default."""
-        ...
     @overload
-    def pop(self, key: _KT) -> _VT:
+    def pop(self, key: KT) -> VT:
         """If L has key return its value and remove it from L, otherwise raise KeyError."""
-        ...
     @overload
-    def pop(self, key: _KT, default: _VT | _T) -> _VT | _T:
+    def pop[T](self, key: KT, default: VT | T) -> VT | T:
         """If L has key return its value and remove it from L, otherwise return default."""
-        ...
-    def popitem(self, least_recent: bool = True) -> tuple[_KT, _VT]:
+    def popitem(self, least_recent: bool = True) -> tuple[KT, VT]:
         """Returns and removes a (key, value) pair. The pair returned is the
         least-recently used if least_recent is true, or the most-recently used if false."""
-        ...
-    def keys(self) -> list[_KT]:
+    def keys(self) -> list[KT]:
         """List of L's keys in MRU order."""
-        ...
-    def values(self) -> list[_VT]:
+    def values(self) -> list[VT]:
         """List of L's values in MRU order."""
-        ...
-    def items(self) -> list[tuple[_KT, _VT]]:
+    def items(self) -> list[tuple[KT, VT]]:
         """List of L's items (key, value) in MRU order."""
-        ...
-    def has_key(self, key: _KT) -> bool:
+    def has_key(self, key: KT) -> bool:
         """Check if key is there in L."""
-        ...
     def set_size(self, size: int) -> None:
         """Set size of LRU."""
-        ...
     def get_size(self) -> int:
         """Get size of LRU."""
-        ...
     def clear(self) -> None:
         """Clear LRU."""
-        ...
     def get_stats(self) -> tuple[int, int]:
         """Returns a tuple with cache hits and misses."""
-        ...
-    def peek_first_item(self) -> tuple[_KT, _VT] | None:
+    def peek_first_item(self) -> tuple[KT, VT] | None:
         """Returns the MRU item (key, value) without changing key order."""
-        ...
-    def peek_last_item(self) -> tuple[_KT, _VT] | None:
+    def peek_last_item(self) -> tuple[KT, VT] | None:
         """Returns the LRU item (key, value) without changing key order."""
-        ...
     @overload
-    def update(self, mapping: Mapping[_KT, _VT], /) -> None:
+    def update(self, mapping: Mapping[KT, VT], /) -> None:
         """Update value for key in LRU."""
-        ...
     @overload
-    def update(self: LRU[_KT | str, _VT], **kwargs: _VT) -> None:
+    def update(self: LRU[str, VT], **kwargs: VT) -> None:
         """Update value for key in LRU."""
-        ...
     @overload
-    def update(
-        self: LRU[_KT | str, _VT], mapping: Mapping[str, _VT], /, **kwargs: _VT
-    ) -> None:
+    def update(self: LRU[str, VT], mapping: Mapping[str, VT], /, **kwargs: VT) -> None:
         """Update value for key in LRU."""
-        ...
     @overload
-    def set_callback(self, callback: Callable[[_KT, _VT], Any]) -> None:
+    def set_callback(self, callback: Callable[[KT, VT], Any]) -> None:
         """Set a callback to call when an item is evicted."""
-        ...
     @overload
     def set_callback(self, callback: None) -> None:
         """Unset the eviction callback."""
-        ...
